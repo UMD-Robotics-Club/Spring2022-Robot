56d80049be0d115894a84ca3f8d415592c0afd3b
grace-UMD <68656045+grace-UMD@users.noreply.github.com>
2022-03-23 19:46:20 -0500

    Update Combo.ino

--- Microntroller_Code/Snippets/Combo.ino
+++ Microntroller_Code/Snippets/Combo.ino
@@ -1,8 +1,10 @@
+// Use link below for Sonar theory and equation explanation
+// https://create.arduino.cc/projecthub/abdularbi17/ultrasonic-sensor-hc-sr04-with-arduino-tutorial-327ff6
 #include <Servo.h>
 
 Servo myservo; 
 
-int pos;
+int position;
 int triggerpin=10;
 int echopin=11;
 long duration;
@@ -19,24 +21,24 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 5) {
-    int i = pos/5;
-    myservo.write(pos);   
+  for (position = 0; position <= 180; position += 5) {
+    int i = position/5;
+    myservo.write(position);   
     digitalWrite(triggerpin,LOW); 
     delayMicroseconds(2);
     digitalWrite(triggerpin,HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(10);    
     digitalWrite(triggerpin,HIGH);
     duration=pulseIn(echopin,HIGH);
-    x=(duration/2)/29.1;     
+    x=(duration/2)/29.1;               //calculated distance in cm 
     Serial.print("Distance :");
     Serial.print(x);      
-    angleDis[i][0] = pos;
+    angleDis[i][0] = position;              
     angleDis[i][1] = x;
     delay(250);                       
   }
-  for (pos = 180; pos >= 0; pos -= 5) { 
-    myservo.write(pos);              
+  for (position = 180; position >= 0; position -= 5) { 
+    myservo.write(position);              
     delay(15);                       
   }
 }
