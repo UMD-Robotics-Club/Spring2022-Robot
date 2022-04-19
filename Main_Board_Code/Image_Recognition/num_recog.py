f6802011a73bed13b2a294403229dee94adb799a
Cynopolis <Cynopolis@users.noreply.github.com>
2022-04-19 18:10:31 -0500

    Changed color range

--- Main_Board_Code/Image_Recognition/num_recog.py
+++ Main_Board_Code/Image_Recognition/num_recog.py
@@ -43,8 +43,8 @@ class Camera:
         #lower_yellow = np.array([20, 115, 115])
         #upper_yellow = np.array([30, 255, 255])
         # matlab colors
-        lower_yellow = np.array([22, 43, 185])
-        upper_yellow = np.array([37, 178, 255])
+        lower_yellow = np.array([25, 68, 126])
+        upper_yellow = np.array([50, 255, 255])
         # convert the frame from rgb to hsv values
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         # find all of the yellow in the image
