09eef47e66601edd63184acbea2fbd298a7169a0
Kole-Koolmo <Koolm008@d.umn.edu>
2022-04-20 18:58:51 -0500

    motorTest.git

--- Main_Board_Code/Motor_Control/motorTest.py
+++ Main_Board_Code/Motor_Control/motorTest.py
@@ -3,8 +3,8 @@ from motor import drive_train
 from motor import motor
 import time
 
-direction_pin1 = 23  #Board pin 16 23
-direction_pin2 = 24  #Board pin 18 24
+direction_pin1 = 16  #Board pin 16 23
+direction_pin2 = 18  #Board pin 18 24
 speed_pin1 = 32      #Board pin 32 12
 speed_pin2 = 33     #Board pin 33 13
 
@@ -12,30 +12,33 @@ motor1 = motor(direction_pin1, speed_pin1)
 motor2 = motor(direction_pin2, speed_pin2)
 
 drive_train= drive_train(motor1, motor2)
-val = 50
-inc = 5
+val = 15
+incr = 1
 turn_ratio = 0
 turn_incr = 0.1
 timer = time.time()
 print('Going straight')
 while time.time() - timer <= 10:
     time.sleep(0.25)
-    if val >= 100:      #2.5 to first max out
+    if val >= 40:     
         incr = -incr
-    if val <= 0:        #7.5 to first stop
+    if val <= 0:      
         incr = -incr
     val += incr
     drive_train.set_turn_velocity(val/100, turn_ratio)
+
 print("Turning one way")
 turn_ratio += turn_incr
 drive_train.set_turn_velocity(val/100, turn_ratio)
-while time.time() - timer <=20 & time.time() - timer >=10:
+has_turned_neg = False
+while time.time() - timer <=10:
     time.sleep(0.25)
-    if turn_ratio >= 1:      
+    if turn_ratio >= 1:
         turn_incr = -turn_incr
-    if turn_ratio <= -1:        
+    if turn_ratio <= -1:
         turn_incr = -turn_incr
     turn_ratio += turn_incr
+    turn_ratio = round(turn_ratio, 2)
     if (turn_ratio == 0):
         print("Turning the other way")
     drive_train.set_turn_velocity(val/100, turn_ratio)
