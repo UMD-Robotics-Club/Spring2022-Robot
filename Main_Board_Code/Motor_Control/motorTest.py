760105716b95b3764dd1a53d8d7dcdd9524f8bed
Kole-koolmo <Koolm008@d.umn.edu>
2022-04-20 19:08:14 -0500

    updated motorTest for acceleration functions

--- Main_Board_Code/Motor_Control/motorTest.py
+++ Main_Board_Code/Motor_Control/motorTest.py
@@ -8,24 +8,20 @@ direction_pin2 = 18  #Board pin 18 24
 speed_pin1 = 32      #Board pin 32 12
 speed_pin2 = 33     #Board pin 33 13
 
-motor1 = motor(direction_pin1, speed_pin1)
-motor2 = motor(direction_pin2, speed_pin2)
+motor1 = motor(direction_pin1, speed_pin1, max_accel = .04)
+motor2 = motor(direction_pin2, speed_pin2, max_accel = .04)
+motor2.invert_dir_pin()
 
 drive_train= drive_train(motor1, motor2)
 val = 15
-incr = 1
 turn_ratio = 0
 turn_incr = 0.1
 timer = time.time()
 print('Going straight')
+drive_train.set_turn_velocity(.2)
 while time.time() - timer <= 10:
-    time.sleep(0.25)
-    if val >= 40:     
-        incr = -incr
-    if val <= 0:      
-        incr = -incr
-    val += incr
-    drive_train.set_turn_velocity(val/100, turn_ratio)
+    drive_train.set_turn_velocity(0.2, turn_ratio)
+    drive_train.update()
 
 print("Turning one way")
 turn_ratio += turn_incr
@@ -41,6 +37,7 @@ while time.time() - timer <=10:
     turn_ratio = round(turn_ratio, 2)
     if (turn_ratio == 0):
         print("Turning the other way")
-    drive_train.set_turn_velocity(val/100, turn_ratio)
+    drive_train.set_turn_velocity(.15, turn_ratio)
+    drive_train.update()
 
 GPIO.cleanup()
